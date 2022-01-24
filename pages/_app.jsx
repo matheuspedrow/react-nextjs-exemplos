13f7da12cf8d6fa7209b6425db8554b3e13129d2
Matheus Pedro <matheuspedrow@gmail.com>
2022-01-24 14:29:25 -0300

    Importado fonte e configurações

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,16 @@
+import Head from 'next/head'
+
 import '../src/style.css'
 
 function App ({Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Head>
+      <title>Titulo padrão</title>
+    </Head>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,22 @@
+import Head from 'next/head'
 import Title from '../src/componentes/title/Title'
 import Subtitle from '../src/componentes/subtitle/Subtitle'
 
 function HomePage () {
   return (
+    <>
+    <Head>
+      <title>Titulo Diferente da HomePage</title>
+
+    </Head>
+    
     <div>
       <Title>Um Titulo</Title>
       <Subtitle text = "Um Subtitulo" />
       <Title>Outro Titulo</Title>
       <Subtitle text = "Outro Subtitulo" />
     </div>
+    </>
   )
 }
 
