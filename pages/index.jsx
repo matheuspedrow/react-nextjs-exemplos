b426c230b9ba1c7268fe9d826ff95ef37b1e77e8
Matheus Pedro <matheuspedrow@gmail.com>
2022-01-24 15:43:22 -0300

    Creado Use State

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,18 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Title from '../src/componentes/title/Title'
 import Subtitle from '../src/componentes/subtitle/Subtitle'
+import Button from '../src/componentes/button/Button'
+
 
 function HomePage () {
+  const [click, setClick] = useState(0)
+
+  const handleClick = () => {
+    console.log ("HANDLE CLICK")
+    setClick (click +1)
+  }
+
   return (
     <>
     <Head>
@@ -15,6 +25,11 @@ function HomePage () {
       <Subtitle text = "Um Subtitulo" />
       <Title>Outro Titulo</Title>
       <Subtitle text = "Outro Subtitulo" />
+      <Button onClick={handleClick}
+      >
+        Botão Teste
+      </Button>
+      <p>Cliques: {click}</p>
     </div>
     </>
   )
